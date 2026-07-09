cbf450b65799c6b8489ff3d22a24a01c45c29c26
CodeWithMe <73113014+CodingWithAman1@users.noreply.github.com>
2026-07-09 16:19:33 +0530

    Update app2.js

--- app2.js
+++ app2.js
@@ -1,11 +1,12 @@
 async function getTrainLive(trainNumber) {
     if (!trainNumber) {
         const el = document.getElementById("trainNumber");
-        const date = document.getElementById("startDate").value;
+        
         trainNumber = el ? el.value.trim() : "12919";
     }
 
     try {
+        const date = document.getElementById("startDate").value;
         const response = await fetch(`/.netlify/functions/train-live?trainNumber=${trainNumber}&date=${date}`);
 
         if (!response.ok) {
